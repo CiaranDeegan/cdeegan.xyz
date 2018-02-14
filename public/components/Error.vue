01582710006188b5f3098ac416b60b68084a8eae
Ciaran Deegan <ciarandee@gmail.com>
2018-02-13 19:43:14 -0500

    added error kitty

--- public/components/Error.vue
+++ public/components/Error.vue
@@ -2,11 +2,19 @@
     <div class='content has-text-centered'>
         <h1>Seems like we took a wrong turn</h1>
         <a href='..' class='button'>Get me outta here!</a>
+        <figure>
+            <img src='../img/error_kitty.gif'>
+        </figure>
     </div>
 </template>
 
 <style scoped>
 .content {
     padding: 15px;
+}
+
+figure img {
+    max-width: 100%;
+    height: auto;
 }
 </style>
