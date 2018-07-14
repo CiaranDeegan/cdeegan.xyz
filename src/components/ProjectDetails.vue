446549760e08479f6299b2467b7c5717f8674eb2
CiaranDeegan <ciaran.deegan23@mail.dcu.ie>
2018-07-13 21:31:28 -0400

    added card content to projects page

--- src/components/ProjectDetails.vue
+++ src/components/ProjectDetails.vue
@@ -1,11 +1,17 @@
 <template>
-	<div class="box">
-		<h2>{{ project.name }}</h2>
-		<b-taglist class="tags">
-			<b-tag v-for="tag in project.tags" :key="tag" rounded type="is-primary">{{ tag }}</b-tag>
-		</b-taglist>
-		<div v-html="project.description"/>
-	</div>
+	<section>
+		<div class="card">
+			<div class="card-header">
+				<p class="card-header-title">{{ project.name }}</p>
+			</div>
+			<div class="card-content">
+				<b-taglist class="tags">
+					<b-tag v-for="tag in project.tags" :key="tag" rounded type="is-primary">{{ tag }}</b-tag>
+				</b-taglist>
+				<div class="content" v-html="project.description"/>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
@@ -21,8 +27,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-	.box {
-		margin: 2em;
+	.card {
+		height: 100%;
+		background-position: center;
 		.tags {
 			display: inline-block;
 		}
